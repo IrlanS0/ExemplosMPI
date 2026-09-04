f99b6013999847d8915f6ae22c75f340a82b2a13
IrlanS0 <irlanfelipe@academico.ufs.br>
2026-09-04 20:46:39 +0000

    Aula 03

--- mpi1.c
+++ mpi1.c
@@ -5,17 +5,34 @@
 int main(void) {
    int        comm_sz;               /* Número de processos    */
    int        my_rank;               /* Rank   do processo     */
+   int a = 2; 
+   int b = 3; 
 
    /* Inicia MPI */
    MPI_Init(NULL, NULL); 
 
    /* Obter número de processos */
    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz); 
-
-   /* Obter rank (id) do processo */
    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
 
+   int total = 0; 
+   if  (my_rank == 0){
+      total = a + b; 
+   }
+   else if (my_rank == 1){
+      total = b - a; 
+   }
+   else if (my_rank == 2){
+      total = a * b;
+   }
+   else if (my_rank == 3){
+      total = a - b; 
+   }
+
+   /* Obter rank (id) do processo */
+
    printf("Número de processos: %d Rank do processos corrente: %d \n", comm_sz, my_rank);
+   printf("TOTAL = %d\n", total);
 
    /* Encerra MPI */
    MPI_Finalize(); 
--- mpi2.c
+++ mpi2.c
@@ -7,30 +7,36 @@ const int MAX_STRING = 100;
 int main(void) {
    char       message[MAX_STRING];  
    int        comm_size;               
-   int        my_rank;               
+   int        my_rank;   
+   int a = 2, b = 3;
+   int tot = 0;          
 
    MPI_Init(NULL, NULL); 
    MPI_Comm_size(MPI_COMM_WORLD, &comm_size); 
    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank); 
 
 
-   if (my_rank != 0) { 
-      /* Criação da mensagem */
-      sprintf(message, "Msg do processo %d of %d!", my_rank, comm_size); 
-      
-      /* Envio da mensagem para o processo 0 */
-      MPI_Send(message, strlen(message)+1, MPI_CHAR, 0, 0, MPI_COMM_WORLD); 
-   } else {  
+   if (my_rank == 1) tot = b - a;
+   else if (my_rank == 2) tot = a * b;
+   else if (my_rank == 3) tot = a - b; 
+   MPI_Send(&tot, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+   
+   if (my_rank == 0) {  
+      tot = a + b;
       /* Impressão da mensagem do processo centralizador */
       printf("Processo centralizador em execução: %d of %d!\n", my_rank, comm_size);
-      
+      printf("O processo %d fez a seguinte operação: %d\n", my_rank, tot);
+
+      int acc = tot;
       for (int q = 1; q < comm_size; q++) {
          /* Recepção da mensagem do processo q */
-         MPI_Recv(message, MAX_STRING, MPI_CHAR, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-      
+         MPI_Recv(&tot, 1, MPI_INT, q, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
          /* Impressão da mensagem do processo q */
-         printf("O processo %d recebeu a seguinte mensagem: %s\n", my_rank, message);
+         printf("O processo %d recebeu a seguinte mensagem: %d\n", my_rank, tot);
+         acc += tot; 
       } 
+      printf("TOTAL: %d\n", acc); 
    }
 
    /* Finaliza MPI */
